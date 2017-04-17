be0e006c714de1dff01361ee8062c282ab448b38
youngsoo jung <youngsoo.j@gmail.com>
2017-04-17 17:02:58 +0900

    Create application.ts

---
+++ application.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods  } from 'angularfire2';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>
+    {{title}}
+  </h1>
+  <div>
+    list
+  </div>
+  <div>
+    <a [routerLink]="['profile']">profile</a>
+  </div>
+  <router-outlet></router-outlet>
+  `
+})
+export class ApplicationComponent {
+  title = 'app works!';
+  authState = null;
+
+  constructor(public af: AngularFire) {
+
+  }
+
+}
