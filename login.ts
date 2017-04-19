5f9e5a560a7e7a010ebf05a6f7fe66b7c15ba7ac
youngsoo jung <youngsoo.j@gmail.com>
2017-04-19 18:52:24 +0900

    Update login.ts

--- login.ts
+++ login.ts
@@ -1,56 +1,97 @@
-import { Component } from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods  } from 'angularfire2';
+
+/*
+
+/signin?returnUrl=proflie
+other page
+1. throught the auth.gurard, go to signin with returnUrl
+
+/signin
+signin windwow
+1. each social signin buttons
+2. a button to goto signup
+
+/signup
+signup window
+1. agreements of terms
+2. social signup button
+
+signin process
+1. has a state
+  1.1 has a 'users/$uid'
+    1.1.1 yes : do signin
+      1.1.1.1 has a returnUrl => move to the returnUrl 
+      1.1.1.2 no returnUrl => move to the /rocket
+    1.1.2 no : goto signup
+2. no state
+  2.1 stay in signin without moving
+
+signup process
+1. did agree on terms
+  1.1 yes : store user data into the '/users' by uid key
+    1.1.1 move to the second step, setting profile or cancle ( goto the /rocket) 
+    1.1.2 move to the third step, setting noti_rocket filter
+  1.2 no : alert for checking terms
+
+*/
+
+import { Component, Inject  } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseRef  } from 'angularfire2';
 import { Router, ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs/Rx";
+import 'rxjs/add/operator/take';
 
 
 @Component({
   selector: 'app-root',
   template: `
-
   <div> {{ (af.auth | async)?.uid }} </div>
   <button (click)="login()">Login</button>
   <button (click)="logout()">Logout</button>
   <br>
   <button (click)="facebookLogin()">FaceBook Login</button>
+ <a [routerLink]="['/signup']" [queryParams]="{debug: true}" fragment="education">
+  signup
+</a>
   `
 })
+
 export class LoginComponent {
-  title = 'Login';
-  authState = null;
-
-  private subscription: Subscription;
-
+  title:string = 'Login';
   param: string;
-
 
   constructor(public router: Router, private route: ActivatedRoute, public af: AngularFire) {
     this.af.auth.subscribe(state => {
-      console.log('login :', state);
-      this.authState = state;
-
-
-          this.subscription = route.queryParams.subscribe(
-              (queryParam: any) => {
-                this.param = queryParam['returnUrl'];
-                if(this.param)
-                  this.router.navigate([this.param]);
-              }
-          );
-          console.log('login param :', this.param);
-
-
-      // state.auth.updateProfile({
-      //   displayName: 'display name s',
-      //   photoURL: "https://example.com/jane-q-user/profile.jpg"
-      // }).then(() => {
-      //   console.log("state 2 :", state);
-      // });
+      if(state){ this.procWithState(state); } 
+      else { console.log("no state"); }
     });
 
-            //this.router.navigate(['/profile']);
+    route.queryParams.subscribe(
+        (queryParam: any) => { this.param = queryParam['returnUrl']; }
+    );
+    console.log('login param :', this.param);
+  }
 
+  procWithState(state){
+    console.log('>> procWithState > state :', state);
+    var uid = state.auth.uid;
+        
+    let item = this.af.database.object('/users/' + uid, { preserveSnapshot: true }).take(1);
+    item.subscribe(snapshot => {
+      let se =  snapshot.exists();
+      console.log("snapshot exists", se);
+      if(se){
+        if(this.param) this.router.navigate([this.param]);
+        else this.router.navigate(['/']);
+      }
+      else{ 
+        this.af.auth.logout();
+        this.router.navigate(['/signup']); 
+      }
+    });
+  }
 
+  procWithoutState(){
+    console.log(">> procWithoutState");
   }
 
   login() {
